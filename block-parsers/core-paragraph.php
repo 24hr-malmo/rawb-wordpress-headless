4679b4387c8fc1c8b82a0f22a82e3e70ac0ef671
Camilo Tapia <camilo.tapia@gmail.com>
2020-05-30 11:17:48 +0200

    upgraded handling of the core/paragraph block

--- block-parsers/core-paragraph.php
+++ block-parsers/core-paragraph.php
@@ -3,7 +3,21 @@
     add_filter('headless_helper__core/paragraph', function($block) {
 
         $parsed_block = new stdclass();
-        $parsed_block->content = $block->innerHTML;
+        $doc = new DOMDocument();
+        $doc->loadHTML($block->innerHTML);
+        $selector = new DOMXPath($doc);
+        $result = $selector->query('//p');
+
+        // This makes sure we allow the shy tag
+        $parsed_block->content = preg_replace(['/&lt;/', '/&amp;shy;/'], ['<', '&shy;'], utf8_decode($result[0]->nodeValue));
+        $parsed_block->tag = 'p';
+
+        // Autocopy all attributes
+        foreach ($block->attrs as $key => $value) {
+            $parsed_block->{$key} = $block->attrs[$key];
+        }
+
         return $parsed_block;
 
     });
+
