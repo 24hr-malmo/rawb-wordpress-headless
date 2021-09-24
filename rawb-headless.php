cbad272caec1e8e65f2bb07a69d43147742b0337
camilo <camilo.tapia@gmail.com>
2021-09-24 09:07:49 +0200

    bump version

--- rawb-headless.php
+++ rawb-headless.php
@@ -4,7 +4,7 @@
     Plugin Name: RAWB Headless Helper
     Plugin URI: http://24hr.se
     Description: Saves content to a Draft Content Service and gives the possibility to push the content to live
-    Version: 0.8.13
+    Version: 0.8.14
     Author: Camilo Tapia <camilo.tapia@24hr.se>
     */
 
